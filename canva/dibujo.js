259236f21f8bd89bb7c6a84076ca668981e9ff4c
Keidy Mercado <92052742+kemesy07@users.noreply.github.com>
2022-01-30 21:29:11 -0500

    ciclos while y for en JS

--- canva/dibujo.js
+++ canva/dibujo.js
@@ -1,8 +1,21 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
 
-dibujarLinea("red", 10, 300, 220, 10);
-dibujarLinea("black", 300, 10, 10, 220);
+while (l < lineas)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea " + l);
+    l = l + 1;
+}
+
+dibujarLinea(colorcito, 1,1,1,299);
+dibujarLinea(colorcito, 1,299,299,299);
 
 function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
 {
